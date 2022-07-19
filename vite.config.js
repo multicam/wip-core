dff6e276317f1478aceac51a036130892f1d523f
multicam <jmgiorgi@gmail.com>
2022-07-20 08:24:18 +1000

    -- works

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite';
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
 
+/** @type {import('vite').UserConfig} */
 const config = {
-
+  plugins: [sveltekit()],
+    server: {
+      fs: {
+        allow: [ '.yarn', 'node_modules' ]
+      }
+    },
   kit: {
     adapter: adapter(),
 
