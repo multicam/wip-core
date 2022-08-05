a9ca6251a89b190fec379406cc374d2555a96ace
multicam <jmgiorgi@gmail.com>
2022-08-05 13:13:43 +1000

    -- feeds & cache /3

--- src/routes/inbox@query.svelte
+++ src/routes/inbox@query.svelte
@@ -2,8 +2,8 @@
   import Table from '$lib/components/table-query.svelte'
 </script>
 <Table
-        type="person"
+        type="inbox-entry"
         filter="persons"
         preorder="desc"
-        uri="http://jmbox.local:5050/v1/crm/persons"
+        uri="http://jmbox.local:5050/v1/feeds/inbox"
 />
